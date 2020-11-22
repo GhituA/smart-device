06e4c5e320a09e3de6826f7260977dab6c4e229d
GhituA <isthatpawz@gmail.com>
2020-11-23 01:48:14 +0200

    Убирает "магические значения" в константы

--- source/js/form.js
+++ source/js/form.js
@@ -3,9 +3,15 @@
 (function () {
   var MIN_NAME_LENGTH = 4;
   var MAX_NAME_LENGTH = 30;
+  var phoneFocusMask = '+7 (';
   var cbPhoneMask = {
-    mask: '+{7}(000)000-00-00'
+    mask: '+{7} (000) 000-00-00'
   };
+
+  var phoneErrorMessage = 'Пример ввода:  +7 (000) 000-00-00';
+  var requiredMessage = 'Это поле обязательно для заполнения';
+  var minLengthMessage = 'Минимальная длина ввода: 4 символа. Введите ещё ';
+  var maxLengthMessage = 'Максимальная длина ввода: 30 символов. Удалите лишние ';
 
   var cbForm = document.querySelector('form');
   var cbPhone = cbForm.querySelector('#phone');
@@ -13,7 +19,7 @@
 
   var onPhoneFocus = function (evt) {
     if (evt.target.value === '') {
-      evt.target.value = '+7 (';
+      evt.target.value = phoneFocusMask;
     }
   };
 
@@ -21,7 +27,7 @@
     var newMask = new window.IMask(evt.target, cbPhoneMask);
 
     if (evt.target.validity.patternMismatch) {
-      evt.target.setCustomValidity('Пример ввода:  +7 (000) 000-00-00');
+      evt.target.setCustomValidity(phoneErrorMessage);
     } else {
       evt.target.setCustomValidity('');
     }
@@ -31,11 +37,11 @@
   var onNameInput = function (evt) {
     var valueLength = evt.target.value.length;
     if (valueLength < 1) {
-      evt.target.setCustomValidity('Это поле обязательно для заполнения');
+      evt.target.setCustomValidity(requiredMessage);
     } else if (valueLength < MIN_NAME_LENGTH) {
-      evt.target.setCustomValidity('Минимальная длина ввода: 4 символов. Введите ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+      evt.target.setCustomValidity(minLengthMessage + (MIN_NAME_LENGTH - valueLength) + ' симв.');
     } else if (valueLength > MAX_NAME_LENGTH) {
-      evt.target.setCustomValidity('Максимальная длина ввода: 30 символов. Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) + ' симв.');
+      evt.target.setCustomValidity(maxLengthMessage + (valueLength - MAX_NAME_LENGTH) + ' симв.');
     } else {
       evt.target.setCustomValidity('');
     }
