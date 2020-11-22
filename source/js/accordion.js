b99a2e6bc3062fda2bffdba2d6574b0baa1434de
GhituA <isthatpawz@gmail.com>
2020-11-23 01:49:43 +0200

    Правит поведение табов

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -6,32 +6,39 @@
   var footerNav = footer.querySelector('.footer-nav');
   var footerNavBar = footer.querySelector('.footer-nav__bar');
   var footerNavLinks = footerNav.querySelector('.footer-nav__links');
-  var footerNavBtn = footerNav.querySelector('.footer-nav__toggle');
   var footerContacts = footer.querySelector('.footer-contacts');
   var footerContactsBar = footer.querySelector('.footer-contacts__bar');
   var footerContactsInfo = footerContacts.querySelector('.footer-contacts__info');
-  var footerContactsBtn = footerContacts.querySelector('.footer-contacts__toggle');
 
+  var onNavClick = function () {
+    if (footerContactsBar && footerContacts.classList.contains('footer-contacts--open')) {
+      footerContacts.classList.remove('footer-contacts--open');
+    }
+    footerNav.classList.toggle('footer-nav--open');
+  };
 
-  if (footerNavBar && footerNav && footerNavBtn) {
-    footerNavLinks.classList.add('footer-nav__links--closed');
-    footerNavBtn.classList.remove('footer-nav__toggle--hidden');
+  var onContactsClick = function () {
+    if (footerNavBar && footerNav.classList.contains('footer-nav--open')) {
+      footerNav.classList.remove('footer-nav--open');
+    }
+    footerContacts.classList.toggle('footer-contacts--open');
+  };
 
-    footerNavBtn.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      footerNavBtn.classList.toggle('footer-nav__toggle--open');
-      footerNavLinks.classList.toggle('footer-nav__links--closed');
+  if (footerNavBar && footerNavLinks) {
+    footerNavBar.classList.add('footer-nav__bar--js');
+    footerNav.classList.remove('footer-nav--open');
+
+    footerNavBar.addEventListener('click', function () {
+      onNavClick();
     });
   }
 
-  if (footerContactsBar && footerContactsInfo && footerContactsBtn) {
-    footerContactsInfo.classList.add('footer-contacts__info--closed');
-    footerContactsBtn.classList.remove('footer-contacts__toggle--hidden');
+  if (footerContactsBar && footerContactsInfo) {
+    footerContactsBar.classList.add('footer-contacts__bar--js');
+    footerContacts.classList.remove('footer-contacts--open');
 
-    footerContactsBtn.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      footerContactsBtn.classList.toggle('footer-contacts__toggle--open');
-      footerContactsInfo.classList.toggle('footer-contacts__info--closed');
+    footerContactsBar.addEventListener('click', function () {
+      onContactsClick();
     });
   }
 })();
